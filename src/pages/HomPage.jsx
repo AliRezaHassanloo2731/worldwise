f65a274082ef47c5d4ad49b29fd032d567c5fdeb
AliRezahassnloo2731 <reza1632731@gmail.com>
2025-07-28 22:44:08 +0200

    add pages and react router and create link between them

---
+++ src/pages/HomPage.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function HomPage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>Home Page</h1>
+    </div>
+  );
+}
+
+export default HomPage;
